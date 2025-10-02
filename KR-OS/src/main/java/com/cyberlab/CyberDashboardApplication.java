9bfee645faa5ec2d19716160b86bf830cf22c575
malekk2006 <malekalastal813@gmail.com>
2025-10-02 15:18:35 +0300

    Create CyberDashboardApplication.java

---
+++ KR-OS/src/main/java/com/cyberlab/CyberDashboardApplication.java
@@ -0,0 +1,11 @@
+package com.cyberlab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyberDashboardApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CyberDashboardApplication.class, args);
+    }
+}
